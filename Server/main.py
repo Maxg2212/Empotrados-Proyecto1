e7ea969d41eec8cc60bf96122624c1e1ba57af57
NJohnsonCR <naheemjohnson@estudiantec.cr>
2025-04-17 19:44:36 -0600

    Agregado metodo de login

--- Server/main.py
+++ Server/main.py
@@ -1,7 +1,31 @@
-from flask import Flask 
+from flask import Flask, jsonify, request, send_file, make_response
+from flask_cors import CORS
+import bcrypt
+import os
+ 
 app = Flask(__name__)
+app.config["DEBUG"] = True
+cors = CORS(app)
+#main.config["CORS_HEADERS"] = "Content-Type"
 
-@app.route('/')
-def home():
-    return "Hello, Flask!"
+storedUsername = "admin"
+storedPassword = "1234"
+
+hashedPassword = bcrypt.hashpw(storedPassword.encode('utf-8'), bcrypt.gensalt())
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+
+    username = data.get('username')
+    password = data.get('password')
+
+    if (username == storedUsername) & bcrypt.checkpw(password.encode('utf-8'), hashedPassword):
+        response = make_response(jsonify({"message": "Login successful"}), 200)
+
+    else:
+        response = make_response(jsonify({"message": "Invalid username or password"}), 401)
+
+    response.headers['Content-Type'] = 'application/json; charset=utf-8'
+    return response
 
